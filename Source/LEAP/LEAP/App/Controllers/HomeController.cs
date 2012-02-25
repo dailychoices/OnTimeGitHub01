c5dcf536554a0917f247cd52f4ee91c2b5c78713
Henry Lee <dailychoices@gmail.com>
2012-02-25 13:46:16 -0500

    init commit

---
+++ Source/LEAP/LEAP/App/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PetaPoco;
+using LEAP.App.Model;
+
+namespace LEAP.App.Controllers
+{
+    public class HomeController : Controller
+    {
+        Database _db;
+
+        public HomeController()
+        {
+            _db = new Database("LeapConnectionString");
+        }
+
+        public ActionResult Index(int id = 1)
+        {
+            TestTable question = _db.SingleOrDefault<TestTable>("SELECT * FROM Test.TestTable WHERE Id = @0", id);
+            return View(question);
+        }
+    }
+}
---
+++ Source/LEAP/LEAP/App/Model/TestTable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LEAP.App.Model
+{
+    [PetaPoco.TableName("Test.TestTable")]
+    [PetaPoco.PrimaryKey("Id")]
+    public partial class TestTable
+    {
+        public int Id { get; set; }
+        public string Desc { get; set; }
+    }
+}
